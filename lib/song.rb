a8eddfe80f58a4195f65828c3e5d8e0b069410e8
Achsah Abraham <achsahmp@gmail.com>
2018-08-23 15:36:49 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,8 +28,11 @@ class Song
     song.save
     song
   end
-  
+
   def self.find_by_name(name)
     self.all.detect { |song| song.name == name}
   end
+
+  def self.find_or_create_by_name
+    self.find_by_name(name) || self.create_by_name(name)
 end
